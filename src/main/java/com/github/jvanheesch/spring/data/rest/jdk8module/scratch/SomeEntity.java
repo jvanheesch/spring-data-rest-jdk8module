bc9876233eb6654a2daf7487ee5d6577bddebcb6
jvanheesch <joris.vanheesch@ae.be>
2020-01-28 14:32:19 +0100

    Removed @JsonUnwrapped

--- src/main/java/com/github/jvanheesch/spring/data/rest/jdk8module/scratch/SomeEntity.java
+++ src/main/java/com/github/jvanheesch/spring/data/rest/jdk8module/scratch/SomeEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 public class SomeEntity {
     @Id
     private Long id;
-    @JsonUnwrapped
+    // @JsonUnwrapped
     @Embedded
     private SomeEmbeddable someEmbeddable;
 
