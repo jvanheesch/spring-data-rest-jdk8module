610e5b5377670f102edf709759aa34fb0645de94
jvanheesch <joris.vanheesch@ae.be>
2020-03-01 16:16:31 +0100

    TODO: kunnen we hier iets mee?

--- src/main/java/com/github/jvanheesch/spring/data/rest/jdk8module/SpringRestConfigurer.java
+++ src/main/java/com/github/jvanheesch/spring/data/rest/jdk8module/SpringRestConfigurer.java
@@ -1,24 +1,22 @@
 package com.github.jvanheesch.spring.data.rest.jdk8module;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.module.SimpleDeserializers;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.module.SimpleSerializers;
+import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
+import com.fasterxml.jackson.databind.ser.impl.UnwrappingBeanSerializer;
+import com.fasterxml.jackson.databind.ser.std.BeanSerializerBase;
+import com.fasterxml.jackson.databind.util.NameTransformer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @Component
 public class SpringRestConfigurer implements RepositoryRestConfigurer {
@@ -30,127 +28,48 @@ public class SpringRestConfigurer implements RepositoryRestConfigurer {
 
     /**
      * https://github.com/spring-projects/spring-data-rest/blob/master/src/main/asciidoc/custom-jackson-deserialization.adoc
+     * https://stackoverflow.com/a/29047973/1939921
      */
-//    @Bean
-//    public SimpleModule customSerializationOptionsModule(VerdictRepository verdictRepository) {
-//        return new CustomSerializationOptionsModule(verdictRepository);
-//    }
+    @Bean
+    public SimpleModule customSerializationOptionsModule(VerdictRepository verdictRepository) {
+        return new SimpleModule() {
+            @Override
+            public void setupModule(SetupContext context) {
 
-    public static class CustomSerializationOptionsModule extends SimpleModule {
-        private final VerdictRepository verdictRepository;
+                context.addBeanSerializerModifier(new BeanSerializerModifier() {
+                    @Override
+                    public JsonSerializer<?> modifySerializer(SerializationConfig config, BeanDescription beanDesc, JsonSerializer<?> serializer) {
+                        System.out.println(beanDesc.getBeanClass());
 
-        private CustomSerializationOptionsModule(VerdictRepository verdictRepository) {
-            this.verdictRepository = verdictRepository;
+                        if (VerdictRecord.class.isAssignableFrom(beanDesc.getBeanClass())) {
+                            return new UnwrappingOrderSerializer((BeanSerializerBase) serializer, NameTransformer.NOP);
+                        }
+                        return serializer;
+                    }
+                });
+            }
+        };
+    }
+
+    public static class UnwrappingOrderSerializer extends UnwrappingBeanSerializer {
+        public UnwrappingOrderSerializer(BeanSerializerBase src, NameTransformer transformer) {
+            super(src, transformer);
         }
 
         @Override
-        public void setupModule(SetupContext context) {
-            SimpleSerializers serializers = new SimpleSerializers();
-            SimpleDeserializers deserializers = new SimpleDeserializers();
-
-            serializers.addSerializer(OriginWoodEvaluation.class, new OriginWoodEvaluationSerializer());
-            deserializers.addDeserializer(OriginWoodEvaluation.class, new OriginWoodEvaluationDeserializer(verdictRepository));
-
-            context.addSerializers(serializers);
-            context.addDeserializers(deserializers);
+        public JsonSerializer<Object> unwrappingSerializer(NameTransformer transformer) {
+            return new UnwrappingOrderSerializer(this, transformer);
         }
 
-        public static class OriginWoodEvaluationSerializer extends JsonSerializer<OriginWoodEvaluation> {
-            @Override
-            public void serialize(OriginWoodEvaluation originWoodEvaluation, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-                gen.writeStartObject();
-                if (originWoodEvaluation.getSupplierLocatedInEurope() != null) {
-                    gen.writeFieldName("supplierLocatedInEurope");
-                    if (originWoodEvaluation.getSupplierLocatedInEurope().getVerdict() == null) {
-                        gen.writeNull();
-                    } else {
-                        gen.writeNumber(originWoodEvaluation.getSupplierLocatedInEurope().getVerdict().getId());
-                    }
-                }
-                if (originWoodEvaluation.getFlegtLicense() != null) {
-                    gen.writeFieldName("flegtLicense");
-                    if (originWoodEvaluation.getFlegtLicense().getVerdict() == null) {
-                        gen.writeNull();
-                    } else {
-                        gen.writeNumber(originWoodEvaluation.getFlegtLicense().getVerdict().getId());
-                    }
-                }
-                if (originWoodEvaluation.getCitesLicense() != null) {
-                    gen.writeFieldName("citesLicense");
-                    if (originWoodEvaluation.getCitesLicense().getVerdict() == null) {
-                        gen.writeNull();
-                    } else {
-                        gen.writeNumber(originWoodEvaluation.getCitesLicense().getVerdict().getId());
-                    }
-                }
-                if (originWoodEvaluation.getFromIndonesia() != null) {
-                    gen.writeFieldName("fromIndonesia");
-                    if (originWoodEvaluation.getFromIndonesia().getVerdict() == null) {
-                        gen.writeNull();
-                    } else {
-                        gen.writeNumber(originWoodEvaluation.getFromIndonesia().getVerdict().getId());
-                    }
-                }
-                gen.writeEndObject();
-            }
+        @Override
+        protected void serializeFields(Object bean, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonGenerationException {
+            VerdictRecord order = (VerdictRecord) bean;
+            jgen.writeStringField("paid", "test");
         }
 
-        public static class OriginWoodEvaluationDeserializer extends StdDeserializer<OriginWoodEvaluation> {
-            private final VerdictRepository verdictRepository;
-
-            public OriginWoodEvaluationDeserializer(VerdictRepository verdictRepository) {
-                super(Verdict.class);
-
-                this.verdictRepository = verdictRepository;
-            }
-
-            @Override
-            public OriginWoodEvaluation deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
-                JsonNode jsonNode = jp.getCodec().readTree(jp);
-
-                Set<String> propertiesPresentInJson = StreamSupport.stream(((Iterable<String>) jsonNode::fieldNames).spliterator(), false)
-                        .collect(Collectors.toSet());
-
-                OriginWoodEvaluation originWoodEvaluation = new OriginWoodEvaluation();
-
-                if (propertiesPresentInJson.contains("supplierLocatedInEurope")) {
-                    JsonNode supplierLocatedInEurope = jsonNode.get("supplierLocatedInEurope");
-                    originWoodEvaluation.setSupplierLocatedInEurope(
-                            new VerdictRecord(supplierLocatedInEurope.isNull()
-                                    ? null :
-                                    this.verdictRepository.findById(supplierLocatedInEurope.longValue()).get()
-                            )
-                    );
-                }
-                if (propertiesPresentInJson.contains("flegtLicense")) {
-                    JsonNode flegtLicense = jsonNode.get("flegtLicense");
-                    originWoodEvaluation.setFlegtLicense(
-                            new VerdictRecord(flegtLicense.isNull()
-                                    ? null :
-                                    this.verdictRepository.findById(flegtLicense.longValue()).get()
-                            )
-                    );
-                }
-                if (propertiesPresentInJson.contains("citesLicense")) {
-                    JsonNode citesLicense = jsonNode.get("citesLicense");
-                    originWoodEvaluation.setCitesLicense(
-                            new VerdictRecord(citesLicense.isNull()
-                                    ? null :
-                                    this.verdictRepository.findById(citesLicense.longValue()).get()
-                            )
-                    );
-                }
-                if (propertiesPresentInJson.contains("fromIndonesia")) {
-                    JsonNode fromIndonesia = jsonNode.get("fromIndonesia");
-                    originWoodEvaluation.setFromIndonesia(
-                            new VerdictRecord(fromIndonesia.isNull()
-                                    ? null :
-                                    this.verdictRepository.findById(fromIndonesia.longValue()).get()
-                            )
-                    );
-                }
-                return originWoodEvaluation;
-            }
+        @Override
+        public boolean isUnwrappingSerializer() {
+            return true;
         }
     }
 }
